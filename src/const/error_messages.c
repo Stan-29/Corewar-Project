a42fc416e19a742ff3fb7ff3777d9b033cd2215d
Stan Gaumain <stanislas.gaumain@epitech.eu>
2026-08-29 00:54:09 +0200

    add(start_game.c): check if the number of args is more than 4

--- src/const/error_messages.c
+++ src/const/error_messages.c
@@ -9,5 +9,5 @@ const char *error_messages[] = {
     "Error\n",
     "There should be at least two arguments, use \"corewar -h\""
     " to learn more.\n",
-    ""
+    "There are too many args, there can't be more than four arguments.\n"
 };
--- src/start_game.c
+++ src/start_game.c
@@ -12,6 +12,8 @@ int start_game(int argc, char **argv)
 {
     if (argc <= 1)
         return display_error(ARGS_NEEDED);
+    if (argc > 5)
+        return display_error(TOO_MANY_ARGS);
     if (argc == 2 && is_same_str(argv[1], "-h") == 0)
         return display_instr();
     return 0;
