b270f8efef082282f2eddcb5c849ace86920813d
Stan Gaumain <stanislas.gaumain@epitech.eu>
2026-09-06 01:03:01 +0200

    add(prepare_infos.c): put the robot instr in the arena

--- src/setup-infos/prepare_infos.c
+++ src/setup-infos/prepare_infos.c
@@ -11,6 +11,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void if_same_add(robot_args_t *robots_args, game_infos_t *game_infos,
+    unsigned int *index, unsigned int index_robot)
+{
+    if (*index != robots_args[index_robot].load_adress)
+        return;
+    for (unsigned int index_instr = 0;
+        index_instr < robots_args[index_robot].len_instr; index_instr++) {
+        game_infos->arena[*index % MEM_SIZE] = robots_args[index_robot].
+            instr_list[index_instr];
+        *index += 1;
+    }
+}
+
+void put_robots_in_arena(robot_args_t *robots_args, game_infos_t *game_infos)
+{
+    for (unsigned int index = 0; index < MEM_SIZE; index++) {
+        for (unsigned int index_robot = 0; index_robot < game_infos->nb_robots;
+            index_robot++)
+            if_same_add(robots_args, game_infos, &index, index_robot);
+    }
+    for (unsigned int index_robot = 0; index_robot < game_infos->nb_robots;
+        index_robot++)
+        game_infos->robots_game[index_robot].pc =
+            robots_args[index_robot].load_adress;
+}
+
 unsigned int prepare_infos(robot_args_t *robots_args, game_infos_t **game_infos)
 {
     *game_infos = malloc(sizeof(game_infos_t));
@@ -18,9 +44,10 @@ unsigned int prepare_infos(robot_args_t *robots_args, game_infos_t **game_infos)
         return display_error(MALLOC_FAIL);
     if (init_game_infos(robots_args, *game_infos) == ERROR)
         return ERROR;
-    manage_robots_id((*game_infos)->robots_args, (*game_infos)->nb_robots);
-    if (manage_load_pos((*game_infos)->robots_args,
+    manage_robots_id(robots_args, (*game_infos)->nb_robots);
+    if (manage_load_pos(robots_args,
             (*game_infos)->nb_robots) == ERROR)
         return ERROR;
+    put_robots_in_arena(robots_args, *game_infos);
     return OK;
 }
